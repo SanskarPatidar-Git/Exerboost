144915d6006cfaf503f2856f6e3c7c6d869b30ea
Sanskar <sanskarpatidar14@gmail.com>
2023-04-10 17:13:23 +0530

    splash activity

--- app/src/main/java/com/arbutus/exerboost/activity/SplashActivity.java
+++ app/src/main/java/com/arbutus/exerboost/activity/SplashActivity.java
@@ -1,14 +1,16 @@
-package com.arbutus.exerboost;
+package com.arbutus.exerboost.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.arbutus.exerboost.R;
+
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
     }
 }
