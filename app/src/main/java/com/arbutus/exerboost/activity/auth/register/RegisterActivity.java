828969b1b5e25959ae1768547c33fce61bf75fe0
ADMIN <vanshikarathod14@gmail.com>
2023-04-11 18:40:20 +0530

    pass xml added

---
+++ app/src/main/java/com/arbutus/exerboost/activity/auth/register/RegisterActivity.java
@@ -0,0 +1,17 @@
+package com.arbutus.exerboost.activity.auth.register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.arbutus.exerboost.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_register);
+    }
+}
