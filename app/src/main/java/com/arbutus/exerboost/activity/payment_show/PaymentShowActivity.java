8db6e3f09beaa0ffc24fa01cb395cb751d879566
ADMIN <vanshikarathod14@gmail.com>
2023-04-14 18:44:16 +0530

    toshowpayment

---
+++ app/src/main/java/com/arbutus/exerboost/activity/payment_show/PaymentShowActivity.java
@@ -0,0 +1,16 @@
+package com.arbutus.exerboost.activity.payment_show;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.arbutus.exerboost.R;
+
+public class PaymentShowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_show);
+    }
+}
