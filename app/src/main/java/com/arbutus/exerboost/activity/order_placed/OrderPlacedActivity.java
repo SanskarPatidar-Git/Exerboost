cb9e3f36525d592ff903366b5ea8acbfdab6cabd
arbutus-android-23 <desksurecowork@gmail.com>
2023-04-14 16:59:49 +0530

    yourorderplacedscreenadded

---
+++ app/src/main/java/com/arbutus/exerboost/activity/order_placed/OrderPlacedActivity.java
@@ -0,0 +1,16 @@
+package com.arbutus.exerboost.activity.order_placed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.arbutus.exerboost.R;
+
+public class OrderPlacedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_placed);
+    }
+}
